49bc668c40e21c5c9082a9e4f6c3b8cfdb4a4c13
Thomas Da Silva <thomas-78000@hotmail.fr>
2023-05-30 21:01:01 +0200

    top 100

--- spring_challenge_2023/sources/my_line.cpp
+++ spring_challenge_2023/sources/my_line.cpp
@@ -6,7 +6,7 @@
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/27 22:52:55 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/05/30 17:57:14 by tda-silv         ###   ########.fr       */
+/*   Updated: 2023/05/30 20:31:52 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,26 +28,26 @@ int	which_not_connected(Data &stock_data, int origin, int neighbor)
 
 std::vector<int>	assign_power_until_base(Data &stock_data, int neighbor, int index_base)
 {
-	std::pair<int, std::vector<int> >	path_to_base;
+	std::vector<int>	path_to_base;
 
-	path_to_base = find_next_base(stock_data, neighbor, 30, index_base);
+	path_to_base = find_next_base2(stock_data, neighbor, index_base, 30);
 
 	// Affichage les beacons
-	for (size_t i = 0; i < path_to_base.second.size(); i++)
+	for (size_t i = 0; i < path_to_base.size(); i++)
 	{
-		stock_data.power_cell[path_to_base.second[i]] += 1;
-		cerr << std::setw(3) << path_to_base.second[i] << " │";
+		stock_data.power_cell[path_to_base[i]] += 1;
+		cerr << std::setw(3) << path_to_base[i] << " │";
 	}
 	stock_data.power_cell[index_base] = 1;
 	cerr << endl;
 
 	// Affichage le power des beacons
-	for (size_t i = 0; i < path_to_base.second.size(); i++)
+	for (size_t i = 0; i < path_to_base.size(); i++)
 	{
-		cerr << std::setw(3) << stock_data.power_cell[path_to_base.second[i]] << " │";
+		cerr << std::setw(3) << stock_data.power_cell[path_to_base[i]] << " │";
 	}
 	cerr << endl;
-	return (path_to_base.second);
+	return (path_to_base);
 }
 
 int	my_line(Data &stock_data, int origin, int neighbor, int index_base)
@@ -55,25 +55,29 @@ int	my_line(Data &stock_data, int origin, int neighbor, int index_base)
 	std::vector<int>	path;
 	int					power_prio;
 
-	// assigne priority cell
-	path = find_path_origin_neighbor(stock_data, origin, neighbor, 10);
+//	assigne priority cell
+	path = find_next_beacon(stock_data, neighbor, 10, index_base).second;
 
 	stock_data.total_power_beacon = 0;
 	for (int i = 0; i < stock_data.number_of_cells; i++)
 		if (stock_data.beacon_this_loop[i])
 			stock_data.total_power_beacon += stock_data.power_cell[i] * stock_data.priority_cell[i];
 
-//	if (origin != index_base)
+	cerr << "( " << stock_data.total_power_beacon << " * " << ((path.size() - 1) * 2) << " > " << stock_data.total_ants << " ) = ";
+	cerr << stock_data.total_power_beacon * ((path.size() - 1) * 2) << " > " << stock_data.total_ants << " ? ";
+
+//	if (stock_data.total_power_beacon * ((path.size() - 1) * 2) > (unsigned long int) stock_data.total_ants)
 //	{
-		cerr << "( " << stock_data.total_power_beacon << " * " << ((path.size() - 1) * 2) << " > " << stock_data.total_ants << " ) = ";
-		cerr << stock_data.total_power_beacon * ((path.size() - 1) * 2) << " > " << stock_data.total_ants << " ? ";
+//		cerr << "OUI" << endl;
+//		return (1);
 //	}
-	if (stock_data.total_power_beacon * ((path.size() - 1) * 2) > (unsigned long int) stock_data.total_ants)
+	if (stock_data.beacon * ((path.size() - 1) * 2) > (unsigned long int) stock_data.total_ants)
 	{
 		cerr << "OUI" << endl;
 		return (1);
 	}
 	cerr << "NON" << endl;
+
 //	if (origin == index_base)
 //		if (stock_data.beacon + path.size() > (unsigned long int) stock_data.total_ants * 50 / 100)
 //			return (1);
