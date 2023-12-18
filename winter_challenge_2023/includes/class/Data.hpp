b8bbbfedb690421fcaae3a78868627c872258fc1
Thomas DA SILVA <thomas-78000@hotmail.fr>
2023-12-18 18:06:06 +0100

    sort files

--- winter_challenge_2023/includes/class/Data.hpp
+++ winter_challenge_2023/includes/class/Data.hpp
@@ -6,7 +6,7 @@
 /*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/18 08:57:10 by tda-silv          #+#    #+#             */
-/*   Updated: 2023/12/18 16:33:15 by tda-silv         ###   ########.fr       */
+/*   Updated: 2023/12/18 17:55:46 by tda-silv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,19 @@
 
 using namespace std;
 
+struct s_creature
+{
+	int	id;
+	int	color;	// de 0 à 3
+	int	type;	// de 0 à 2
+
+	int	x;		// position
+	int	y;		//
+
+	int	vx;		// vitesse
+	int vy;		//
+};
+
 class Data
 {
 	public:
@@ -37,8 +50,20 @@ class Data
 		Data(const Data &src);
 		~Data(void);
 
+		int						creature_count;
+		map<int, s_creature>	creatures;
+		int						my_score;
+		int						foe_score;
+		int						my_scan_count;
+		int						foe_scan_count;
+		int						my_drone_count;
+		int						foe_drone_count;
+		int						drone_scan_count;
+		int 					visible_creature_count;
+		int 					radar_blip_count;
+
 	protected:
-	
+
 	private:
 
 };
