cab81a00b9311b738e986559622b7b2e3883405b
Thomas DA SILVA <thomas-78000@hotmail.fr>
2023-12-18 16:37:39 +0100

    new folder and setup files for winter chall 2023

---
+++ winter_challenge_2023/includes/class/Data.hpp
@@ -0,0 +1,46 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Data.hpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/18 08:57:10 by tda-silv          #+#    #+#             */
+/*   Updated: 2023/12/18 16:33:15 by tda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef DATA_HPP
+# define DATA_HPP
+
+# include <iomanip>
+# include <list>
+# include <stdio.h>
+# include <stdlib.h>
+# include <iostream>
+# include <string>
+# include <vector>
+# include <map>
+# include <algorithm>
+# include <queue>
+# include <cmath>
+# include <array>
+# include <ctgmath>
+
+using namespace std;
+
+class Data
+{
+	public:
+	
+		Data(void);
+		Data(const Data &src);
+		~Data(void);
+
+	protected:
+	
+	private:
+
+};
+
+#endif
---
+++ winter_challenge_2023/includes/header.hpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   header.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tda-silv <tda-silv@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/02 15:12:11 by tda-silv          #+#    #+#             */
+/*   Updated: 2023/12/18 16:34:46 by tda-silv         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef HEADER_HPP
+# define HEADER_HPP
+
+# include <iomanip>
+# include <list>
+# include <stdio.h>
+# include <stdlib.h>
+# include <iostream>
+# include <string>
+# include <vector>
+# include <map>
+# include <algorithm>
+# include <queue>
+# include <cmath>
+# include <array>
+# include <random>
+# include <ctgmath>
+
+using namespace std;
+
+# include "./class/Data.hpp"
+
+#endif
