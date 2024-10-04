16a0352573d56d7ba604ffbd5d4bb4c2f02b6a7c
Thomas Da Silva <thomas-78000@hotmail.fr>
2024-10-04 12:25:05 +0200

    action POD

--- fall_challenge_2024/sources/class/Game/actions.cpp
+++ fall_challenge_2024/sources/class/Game/actions.cpp
@@ -1,19 +1,19 @@
 #include "../../../includes/header.hpp"
 
 void	Game::tube(int buildingId1, int buildingId2) {
-	float	distance = data.calculateDistance(buildingId1, buildingId2);
+    float	distance = data.calculateDistance(buildingId1, buildingId2);
 
-	if (distance == -1)
-		return ;
+    if (distance == -1)
+        return ;
 
     int		cost = static_cast<int>(floor(distance * 10));
 
     if (data.resources >= cost) {
         data.resources -= cost;
         data.travel_routes[{buildingId1, buildingId2}].capacity = 1;
-		data.travel_routes[{buildingId1, buildingId2}].cost = cost;
+        data.travel_routes[{buildingId1, buildingId2}].cost = cost;
 
-		cout << "TUBE " << buildingId1 << " " << buildingId2 << ";";
+        cout << "TUBE " << buildingId1 << " " << buildingId2 << ";";
         cerr << "New tube : " << buildingId1 << " - " << buildingId2 << "     cost : " << cost << endl;
     } else {
         cerr << "ERROR TUBE : Failed to creat new tube ( " << buildingId1 << " - " << buildingId2 << " )" << endl;
@@ -29,7 +29,27 @@ void	Game::teleport(int buildingIdEntrance, int buildingIdExit) {
 }
 
 void	Game::pod(int podId, initializer_list<int> buildingIds) {
+    const int podCost = 1000;
 
+    if (data.resources >= podCost && podId >= 1 && podId <= 500) {
+        data.resources -= podCost;
+
+        auto    it = buildingIds.begin();
+        int     firstBuilding = *it;
+        int     lastBuilding = *(buildingIds.end());
+
+        cout << "POD " << podId;
+        for (auto &buildingId : buildingIds)
+            cout << " " << buildingId;
+        cout << ";";
+
+        if (firstBuilding == lastBuilding)
+            cerr << "New Capsule : " << podId << "     LOOP" << std::endl;
+        else
+            cerr << "New Capsule : " << podId << "     LINE" << std::endl;
+    } else {
+        cerr << "ERROR POD" << endl;
+    }
 }
 
 void	Game::destroy(int podId) {
@@ -37,9 +57,9 @@ void	Game::destroy(int podId) {
 }
 
 void	Game::wait() {
-	cout << "WAIT";
+    cout << "WAIT";
 }
 
 void	Game::end() {
-	cout << endl;
+    cout << endl;
 }
--- fall_challenge_2024/sources/main.cpp
+++ fall_challenge_2024/sources/main.cpp
@@ -10,10 +10,8 @@ int main()
 
         game.tube(0, 1);
         game.tube(0, 2);
-        game.pod(42, {0, 1, 0, 2, 0, 1, 0, 2});
+        game.pod(42, {0, 1, 0, 2, 0});
         game.end();
-
-        // cout << "TUBE 0 1;TUBE 0 2;POD 42 0 1 0 2 0 1 0 2" << endl; // TUBE | UPGRADE | TELEPORT | POD | DESTROY | WAIT
     }
 
     return (0);
